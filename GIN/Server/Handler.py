0a40c2ef14069a8ab89a1dc27706b5cd62013407
Félix Gagnon-Grenier <felix.gagnon-grenier@crim.ca>
2019-02-01 13:02:49 -0500

    remove unused code after calling api directly

--- GIN/Server/Handler.py
+++ GIN/Server/Handler.py
@@ -1,6 +1,4 @@
 from GIN.Templating import Renderer
-from GIN.Model.Service.Taxonomy import Taxonomy
-from json import dumps
 
 
 def status_string(code, reason) -> str:
@@ -24,15 +22,3 @@ class Rendering:
 
     def generate_markup_for_section(self) -> [str, list, str]:
         return html_200_response(self.renderer.render('sections/%s.html' % self.section))
-
-
-class API:
-
-    def __init__(self, taxonomy_service: Taxonomy):
-        self.taxonomy_service = taxonomy_service
-
-    def GET_taxonomy(self) -> [str, list, str]:
-        return json_200_response(dumps(self.taxonomy_service.GET_all_taxonomies()))
-
-    def GET_taxonomy_by_id(self, taxonomy_id) -> [str, list, str]:
-        return json_200_response(dumps([self.taxonomy_service.GET_taxonomy_by_id(taxonomy_id)]))
