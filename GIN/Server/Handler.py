8601fb67748551943124715376675c93cbb56fbb
Félix Gagnon-Grenier <felix.gagnon-grenier@crim.ca>
2019-02-04 15:45:21 -0500

    route for changelog

--- GIN/Server/Handler.py
+++ GIN/Server/Handler.py
@@ -1,8 +1,13 @@
 from GIN.Templating import Renderer
+from os import path
 
 
 def status_string(code, reason) -> str:
     return f'{code} {reason}'
+
+
+def plain_text_200_response(content: str) -> [str, list, str]:
+    return status_string(200, 'OK'), [('Content-Type', 'text/plain')], content
 
 
 def json_200_response(content: str) -> [str, list, str]:
@@ -22,3 +27,11 @@ class Rendering:
 
     def generate_markup_for_section(self) -> [str, list, str]:
         return html_200_response(self.renderer.render('sections/%s.html' % self.section))
+
+
+class Rst:
+
+    def send_rst_as_plain_text(self) -> [str, list, str]:
+        file_path = path.dirname(__file__)
+        with open(path.join(file_path, '..', '..', 'CHANGELOG.rst')) as file:
+            return plain_text_200_response(file.read())
