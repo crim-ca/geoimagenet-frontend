d4bbb693cd45eaee866be2f193830d290d8edaf9
Félix Gagnon-Grenier <felix.gagnon-grenier@crim.ca>
2019-03-12 13:13:01 -0400

    chg: sidebar sections in material panels

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
 import {observer} from 'mobx-react';
+import PropTypes from 'prop-types';
+import {
+    createMuiTheme,
+    CssBaseline,
+    MuiThemeProvider,
+    ExpansionPanel,
+    ExpansionPanelSummary,
+    ExpansionPanelDetails,
+} from '@material-ui/core';
+import {ExpandMore} from '@material-ui/icons';
+import {lightBlue, lightGreen} from '@material-ui/core/colors';
+
 import {Actions} from './Actions.js';
 import {TaxonomyClasses, TaxonomySelector} from './TaxonomyBrowser.js';
-import PropTypes from 'prop-types';
 import {MapManager} from '../MapManager.js';
-import {createMuiTheme, CssBaseline, MuiThemeProvider} from '@material-ui/core';
-import {lightBlue, lightGreen} from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
     typography: {
@@ -40,6 +49,10 @@ class Platform extends Component {
         user_interactions: PropTypes.object.isRequired,
     };
 
+    state = {
+        expanded: null,
+    };
+
     constructor(props) {
         super(props);
         this.map_manager = {};
@@ -57,10 +70,18 @@ class Platform extends Component {
         );
     }
 
+    handle_change = panel => (event, expanded) => {
+        this.setState({
+            expanded: expanded ? panel : false,
+        });
+    };
+
     render() {
 
         const taxonomy_class = this.props.state_proxy.flat_taxonomy_classes[this.props.state_proxy.selected_taxonomy.root_taxonomy_class_id];
         const classes = taxonomy_class && taxonomy_class.children ? taxonomy_class.children : [];
+
+        const {expanded} = this.state;
 
         return (
             <MuiThemeProvider theme={theme}>
@@ -73,22 +94,30 @@ class Platform extends Component {
                         <Actions actions_activated={this.props.state_proxy.actions_activated}
                                  mode={this.props.state_proxy.mode}
                                  store_actions={this.props.store_actions} />
-                        <section id='taxonomy' className="taxonomy opened">
-                            <button className="section-handle">Taxonomies and Classes</button>
-                            <TaxonomySelector select_taxonomy={this.props.user_interactions.select_taxonomy}
-                                              taxonomy={this.props.state_proxy.taxonomy} />
-                            <TaxonomyClasses map_manager={this.map_manager}
-                                             user_interactions={this.props.user_interactions}
-                                             store_actions={this.props.store_actions}
-                                             state_proxy={this.props.state_proxy}
-                                             invert_taxonomy_class_visibility={this.props.store_actions.invert_taxonomy_class_visibility}
-                                             toggle_taxonomy_class_tree_element={this.props.store_actions.toggle_taxonomy_class_tree_element}
-                                             classes={classes} />
-                        </section>
-                        <section className="layer-switcher closed">
-                            <button className="section-handle">Basemaps, Images and Filters</button>
-                            <div id="layer-switcher" className="layer-switcher-container" />
-                        </section>
+                        <ExpansionPanel expanded={expanded === 'taxonomies'} onChange={this.handle_change('taxonomies')}>
+                            <ExpansionPanelSummary expandIcon={<ExpandMore/>}>
+                                Taxonomies and Classes
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <TaxonomySelector select_taxonomy={this.props.user_interactions.select_taxonomy}
+                                                  taxonomy={this.props.state_proxy.taxonomy} />
+                                <TaxonomyClasses map_manager={this.map_manager}
+                                                 user_interactions={this.props.user_interactions}
+                                                 store_actions={this.props.store_actions}
+                                                 state_proxy={this.props.state_proxy}
+                                                 invert_taxonomy_class_visibility={this.props.store_actions.invert_taxonomy_class_visibility}
+                                                 toggle_taxonomy_class_tree_element={this.props.store_actions.toggle_taxonomy_class_tree_element}
+                                                 classes={classes} />
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                        <ExpansionPanel expanded={expanded === 'layers'} onChange={this.handle_change('layers')}>
+                            <ExpansionPanelSummary expandIcon={<ExpandMore/>}>
+                                Basemaps, Images and Filters
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <div id="layer-switcher" className="layer-switcher-container" />
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
                     </div>
                 </div>
             </MuiThemeProvider>
