c3d604c982b8c8a2b000615ccb0a6fb61bc43e2f
Félix Gagnon-Grenier <felix.gagnon-grenier@crim.ca>
2019-06-14 17:25:32 -0400

    presentation component in react hook for easier use of i18n

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,29 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import {initReactI18next, useTranslation} from 'react-i18next';
+
 export {notifier} from './notifications.js';
+
+i18n.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        debug: true,
+        interpolation: {
+            escapeValue: false,
+        },
+        resources: {
+            en: {
+                translation: {
+                    'somekey': 'Here is the text for somekey'
+                }
+            },
+            fr: {
+                translation: {
+                    'somekey': 'voici la traduction pour somekey'
+                }
+            }
+
+        }
+    });
+
+export {i18n, useTranslation};
