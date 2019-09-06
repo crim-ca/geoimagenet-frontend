e76eabfa56b2dc14900cbc869a316a095a15707d
Félix Gagnon-Grenier <felix.gagnon-grenier@crim.ca>
2019-09-06 10:14:32 -0400

    usr: GEOIM-254 - toggle checkbox with label click

--- src/components/Platform/Filters/FiltersPaper.js
+++ src/components/Platform/Filters/FiltersPaper.js
@@ -1,11 +1,13 @@
 // @flow strict
 
 import React from 'react';
+import {observer} from 'mobx-react';
 
 import withStyles from "@material-ui/core/styles/withStyles";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 
+import {CheckboxLineInput} from './CheckboxLineInput';
 import {GeoImageNetStore} from "../../../store/GeoImageNetStore";
 import {TFunction} from 'react-i18next';
 import {withTranslation} from '../../../utils';
@@ -24,6 +26,16 @@ const style = theme => {
                 borderTop: `3px solid ${colors.turquoise}`,
             },
             '& > ul > li': {
+                cursor: 'pointer',
+                padding: '0 6px 0 0',
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                transition: 'background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+            },
+            '& > ul > li:hover': {
+                backgroundColor: 'rgba(0, 0, 0, 0.08)',
+                cursor: 'pointer',
                 padding: '0 6px 0 0',
                 display: 'flex',
                 flexDirection: 'row',
@@ -32,7 +44,7 @@ const style = theme => {
             '& input[type=checkbox]': {
                 display: 'inline-block'
             }
-        }
+        },
     };
 };
 
@@ -46,6 +58,7 @@ type Props = {
     toggle_status_filter: () => void,
 };
 
+@observer
 class FiltersPaper extends React.Component<Props> {
     render() {
         const {t, state_proxy, classes, toggle_ownership_filter, toggle_status_filter} = this.props;
@@ -55,13 +68,13 @@ class FiltersPaper extends React.Component<Props> {
                     {
                         Object.keys(state_proxy.annotation_status_filters).map((status_text: string, i: number) => {
                             const status_filter = state_proxy.annotation_status_filters[status_text];
+                            const unique_input_id = `status_${status_text}`;
                             return (
                                 <li key={i}>
-                                    <input type='checkbox'
-                                           checked={status_filter.activated}
-                                           onChange={toggle_status_filter(status_filter.text)} />
-                                    <Typography
-                                        variant='body2'>{t(`annotations:status.${status_filter.text}`)}</Typography>
+                                    <CheckboxLineInput unique_id={unique_input_id}
+                                                       checked={status_filter.activated}
+                                                       change_handler={toggle_status_filter(status_filter.text)}
+                                                       label={t(`annotations:status.${status_filter.text}`)} />
                                 </li>
                             );
                         })
@@ -71,13 +84,13 @@ class FiltersPaper extends React.Component<Props> {
                     {
                         Object.keys(state_proxy.annotation_ownership_filters).map((ownership: string, i: number) => {
                             const ownership_filter = state_proxy.annotation_ownership_filters[ownership];
+                            const unique_input_id = `ownership_${ownership}`;
                             return (
                                 <li key={i}>
-                                    <input type='checkbox'
-                                           checked={ownership_filter.activated}
-                                           onChange={toggle_ownership_filter(ownership_filter.text)} />
-                                    <Typography
-                                        variant='body2'>{t(`annotations:ownership.${ownership_filter.text}`)}</Typography>
+                                    <CheckboxLineInput unique_id={unique_input_id}
+                                                       checked={ownership_filter.activated}
+                                                       change_handler={toggle_ownership_filter(ownership_filter.text)}
+                                                       label={t(`annotations:ownership.${ownership_filter.text}`)} />
                                 </li>
                             );
                         })
