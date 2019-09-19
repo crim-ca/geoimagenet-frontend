4eb8daf74831722f751900bbf2017581958c7f75
Félix Gagnon-Grenier <felix.gagnon-grenier@crim.ca>
2019-09-19 11:58:33 -0400

    usr: GEOIM-267 - showing annotators nicknames or ids

--- src/components/Map/ol_dependant_utils.js
+++ src/components/Map/ol_dependant_utils.js
@@ -3,6 +3,7 @@
 import {Feature} from "ol/Feature";
 import {Circle, Fill, Stroke, Style, Text} from "ol/style";
 import {GeoImageNetStore} from "../../store/GeoImageNetStore";
+import type {FollowedUser} from "../../Types";
 
 type StyleFunction = (Feature, number) => Style | Style[];
 
@@ -37,12 +38,29 @@ function select_style(): Style {
 }
 
 export function create_style_function(color: string, state_proxy: GeoImageNetStore, create_for_select_interaction: boolean = false): StyleFunction {
-    return (feature, resolution) => {
+
+
+    /**
+     * If there is a logged user (it's possible there isn't, people can access the map in anonymous mode)
+     * then we should not be trying to substitute nicknames for ids
+     */
+    const {logged_user} = state_proxy;
+    const nickname_map = {};
+    if (logged_user !== null) {
+        const assign_followed_user = (user: FollowedUser) => {
+            nickname_map[user.id] = user.nickname;
+        };
+        logged_user.followed_users.forEach(assign_followed_user);
+    }
+
+    return (feature: Feature, resolution: number) => {
 
         const {show_labels, show_annotators_identifiers} = state_proxy;
         const taxonomy_class = state_proxy.flat_taxonomy_classes[feature.get('taxonomy_class_id')];
         const label = taxonomy_class.name_en || taxonomy_class.name_fr;
-        const identifier = feature.get('annotator_id').toString();
+        const annotator_id = feature.get('annotator_id').toString();
+        // TODO if we need performance of styling, this check could happen at the create style level, and return a different function instead of making the check here
+        const identifier = nickname_map.hasOwnProperty(annotator_id) ? nickname_map[annotator_id] : annotator_id;
 
         const bits = [];
         if (show_labels) {
