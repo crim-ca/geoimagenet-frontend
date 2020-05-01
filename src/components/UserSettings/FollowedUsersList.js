a958c8c38f140cc58c294eba2ebe7fcd74c9acff
pelletfr <francis.pelletier@crim.ca>
2020-05-01 15:35:26 -0400

    Fixes missing translation link

--- src/components/UserSettings/FollowedUsersList.js
+++ src/components/UserSettings/FollowedUsersList.js
@@ -1,13 +1,13 @@
 // @flow strict
 
 import React from 'react';
-import type { FollowedUser } from '../../Types';
 import MaterialTable from 'material-table';
 import { TFunction } from 'react-i18next';
-import { withTranslation } from '../../utils';
-import { tableIcons } from '../../utils/react';
 import ClearIcon from '@material-ui/icons/Clear';
 import { observer } from 'mobx-react';
+import { withTranslation } from '../../utils';
+import { tableIcons } from '../../utils/react';
+import type { FollowedUser } from '../../Types';
 
 type Props = {
   followed_users: FollowedUser[],
@@ -25,7 +25,7 @@ class FollowedUsersList extends React.Component<Props> {
      * followed users is an mobx managed collection. since we are in strict mode, mobx prevents modifications to the value
      * however, it seems that material-table adds meta data to the data we pass to it. so we make a clone and pass that to the table
      */
-    const followed_users_clone = followed_users.map(user => Object.assign({}, user));
+    const followed_users_clone = followed_users.map((user) => ({ ...user }));
     return (
       <div style={{ maxWidth: '100%' }}>
         <MaterialTable
@@ -33,11 +33,11 @@ class FollowedUsersList extends React.Component<Props> {
           columns={[
             {
               title: t('settings:id'),
-              field: 'id'
+              field: 'id',
             },
             {
               title: t('settings:nickname'),
-              field: 'nickname'
+              field: 'nickname',
             },
           ]}
           data={followed_users_clone}
@@ -46,12 +46,13 @@ class FollowedUsersList extends React.Component<Props> {
               icon: clear_icon,
               tooltip: t('settings:delete_user'),
               onClick: (event, rowData) => this.props.delete_user(rowData.id),
-            }
+            },
           ]}
           options={{
-            actionsColumnIndex: -1
+            actionsColumnIndex: -1,
           }}
-          title={t('annotations:ownership.followed_users')} />
+          title={t('status:plural.followed_users')}
+        />
       </div>
     );
   }
