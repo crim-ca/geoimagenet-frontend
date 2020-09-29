b66834365c475fbd64f697ed890459a29828ba95
Félix Gagnon-Grenier <felix.gagnon-grenier@crim.ca>
2020-09-29 10:37:33 -0400

    normalizing one column layout use

--- src/components/Models/Models.js
+++ src/components/Models/Models.js
@@ -1,28 +1,10 @@
 // @flow strict
 
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core';
 import { ModelsTable } from './ModelsTable';
 import { UploadForm } from './UploadForm';
 import { BenchmarksTable } from './BenchmarksTable';
-
-const Grid = withStyles(({ values }) => ({
-  root: {
-    display: 'grid',
-    gridTemplateColumns: `${values.gutterMedium} 1fr minmax(300px, max-content) 1fr ${values.gutterMedium}`,
-    gridTemplateRows: 'min-content',
-  },
-  content: {
-    gridColumn: '3/4',
-    display: 'grid',
-    gridTemplateRows: 'min-content',
-    gridGap: values.gutterSmall,
-  }
-}))(({ classes: { root, content }, children }) => (
-  <div className={root}>
-    <div className={content}>{children}</div>
-  </div>
-));
+import {OneColumnLayout} from '../OneColumnLayout';
 
 type Props = {
   model_upload_instructions_url: string,
@@ -32,11 +14,11 @@ export class Models extends Component<Props> {
 
   render() {
     return (
-      <Grid>
+      <OneColumnLayout>
         <UploadForm model_upload_instructions_url={this.props.model_upload_instructions_url} />
         <ModelsTable />
         <BenchmarksTable />
-      </Grid>
+      </OneColumnLayout>
     );
   }
 }
